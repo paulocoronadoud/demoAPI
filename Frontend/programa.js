5a70c4b3ad33ad79a6ce493615af066e21bdb021
Paulo César Coronado <paulocoronado@gmail.com>
2026-02-12 20:54:11 -0500

    v 0.2

---
+++ Frontend/programa.js
@@ -0,0 +1,9 @@
+//Frontend mínimo
+let url= "http://127.0.0.1:8000/sumar";
+
+let myAPI= url + "?a=5&b=45";
+
+async function crearPeticion(){
+    let response = await fetch(myAPI);
+    let datos = response.json();
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Proyecto demo básico de API Backend en Python
 
+## Para el Backend
+
+1. Crear un ambiente virtual
+2. Instalar la biblioteca fastapi
